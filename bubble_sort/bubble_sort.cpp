bdcb7c5c5b26c9e5f91db8e852611ee73008ed39
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-09-09 21:03:22 +0300

    Add project files.

---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    srand(time(0));
+    const int n = 10;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100;
+    }
+
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printArray(arr, n);
+
+    return 0;
+}
